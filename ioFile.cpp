5f509b0fc0f61e9754292cda48815cb04238cd50
Fanshi13 <aquillafazanaufan@gmail.com>
2025-06-09 10:06:04 +0700

    implementasi input ke file dengan perintah keluar 'q'

--- ioFile.cpp
+++ ioFile.cpp
@@ -7,4 +7,17 @@ int main()
 {
     string baris;
     ofstream outfile;
+
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q")
+            break;
+        outline << baris << endl;
+    }
 }
